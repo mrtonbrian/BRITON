67c5e0c9f1ed5b481a4d37639a1c2639af1fc9d4
Brian Ton <mrtonbrian@gmail.com>
2019-08-17 07:27:27 +0000

    Fixed Server Backend Code

--- flask_app.py
+++ flask_app.py
@@ -1,18 +1,18 @@
-import chess
-import chess.engine as e
-import chess.pgn
-import io
+import sys
 import os
 import os.path
-import sys
-from flask import Flask, render_template, request, jsonify
+import io
+import chess.pgn
+import chess.engine as e
+import chess
+
+from flask import Flask, render_template, request, jsonify, send_from_directory
 
 application = Flask(__name__)
 
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
 
 ENGINE_FILE = '/chess.exe'
-
 
 @application.route('/')
 def main_page():
@@ -27,16 +27,20 @@ def move():
     pgn = r['pgn']
     c = chess.pgn.read_game(io.StringIO(pgn))
 
-    engine = e.SimpleEngine.popen_uci(os.path.dirname(os.path.abspath(__file__)) + ENGINE_FILE)
+    engine = e.SimpleEngine.popen_uci(os.path.dirname(os.path.abspath(__file__))+ENGINE_FILE)
     board = chess.Board()
     if c:
         for i in c.mainline_moves():
             board.push(i)
     res = engine.play(board, e.Limit(time=time), info=chess.engine.INFO_ALL)
     engine.quit()
-    return jsonify(
-        {'move': str(res.move), 'from': str(res.move)[0:2], 'to': str(res.move)[2:], 'eval': str(res.info['score'])})
+    return jsonify({'move': str(res.move), 'from': str(res.move)[0:2], 'to': str(res.move)[2:], 'eval': str(res.info['score'])})
 
+@application.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(application.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     application.run(host="0.0.0.0", port=int("5000"))
+
